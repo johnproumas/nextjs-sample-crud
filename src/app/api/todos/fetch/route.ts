aa0fe8368dbed959ea773b5b6029f75773637510
johnproumas <iproumas@gmail.com>
2023-10-09 14:46:39 +0300

    force-dynamic in GET route handler

--- src/app/api/todos/fetch/route.ts
+++ src/app/api/todos/fetch/route.ts
@@ -1,6 +1,8 @@
 import { db } from "@/db";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const todos = await db.todo.findMany();
